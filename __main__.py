1baa650f8cd083eb601b00ca963fc2faadd4616e
Ivo Meixner <natiwastaken@gmail.com>
2017-07-10 21:21:21 +0200

    Implemented simply algorithm to test the idea

--- __main__.py
+++ __main__.py
@@ -2,13 +2,45 @@
 
 import random
 
-INIT_CELL_COUNT = 100
+INIT_CELL_COUNT = 100000
+TARGET_ITERATIONS = 100000
+CELL_SPLIT_LIMIT = 1.0
 
-rand = random.Random()
+RAND = random.Random()
+
 cells = []
 
 for i in range(INIT_CELL_COUNT):
-    cells.append(rand.random())
-    print(cells[i])
+    cells.append(RAND.random())
 
-print(len(cells))
+for i in range(TARGET_ITERATIONS):
+    attacker = RAND.randrange(0, len(cells))
+
+    defender = attacker
+    while defender == attacker:
+        defender = RAND.randrange(0, len(cells))
+
+    if cells[attacker] >= cells[defender]:
+        cells[attacker] += cells[defender]
+
+        if cells[attacker] >= CELL_SPLIT_LIMIT:
+            new_value = cells[attacker] / 2.0
+            cells[attacker] = new_value
+            cells.append(new_value)
+
+        cells.pop(defender)
+
+    else:
+        cells[defender] += cells[attacker]
+
+        if cells[defender] >= CELL_SPLIT_LIMIT:
+            new_value = cells[defender] / 2.0
+            cells[defender] = new_value
+            cells.append(new_value)
+
+        cells.pop(attacker)
+
+    if not i % 100:
+        print(i // 100, len(cells), sum(cells))
+
+print("Done!")
