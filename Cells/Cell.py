2bd9d1ee11c1e5c2ce3c1127d7b8546bf8dd29fc
Ivo Meixner <ivo.meixner@se7en.cz>
2017-07-13 08:57:38 +0200

    Implemented a new more natural algorithm for cell behavior

--- Cells/Cell.py
+++ Cells/Cell.py
@@ -2,15 +2,31 @@
 
 class Cell:
     """This class stores information about a single cell."""
-    position = 0.0
-    velocity = 0.0
-    mass = 0.0
 
-    def __init__(self, position_init, velocity_init, mass_init):
+    def __init__(self, position_init, max_velocity_init, mass_init):
         self.position = position_init
-        self.velocity = velocity_init
+        self.max_velocity = max_velocity_init
         self.mass = mass_init
+        self.ready = False
 
-    def move(self):
-        """This method moves the cell as if a single time unit passed."""
-        self.position += self.velocity
+    def attack(self, target):
+        """This method moves this cell as close to another cell as possible
+        and eats the target cell if its within this cell's reach."""
+        # Calculate position difference between the cells
+        pos_diff = target.position - self.position
+
+        self.ready = False
+
+        # The distance is lower than the Cell's velocity
+        if abs(pos_diff) < self.max_velocity:
+            self.position += pos_diff
+            self.mass += target.mass
+            return True
+        # The distance is higher and the position is higher
+        elif pos_diff > 0.0:
+            self.position += self.max_velocity
+            return False
+        # The distance is higher and the position is lower
+        else:
+            self.position -= self.max_velocity
+            return False
--- Cells/__main__.py
+++ Cells/__main__.py
@@ -5,33 +5,67 @@ import constants as const
 import Cell
 import numpy as np
 
-def main():
-    """The main function of the script."""
-    cells = []
-
-    for _ in range(const.INIT_CELL_COUNT):
-        cells.append(Cell.Cell(
-            np.random.uniform(-const.INIT_CELL_POSITION, const.INIT_CELL_POSITION),
-            np.random.uniform(-const.INIT_CELL_VELOCITY, const.INIT_CELL_VELOCITY),
-            np.random.uniform(-const.INIT_CELL_MASS, const.INIT_CELL_MASS)))
-
-    for i in range(const.TARGET_ITERATIONS):
-        max_pos = 0.0
-        min_pos = 0.0
-
-        for cel in cells:
-            cel.move()
-
-            if cel.position > max_pos:
-                max_pos = cel.position
-
-            if cel.position < min_pos:
-                min_pos = cel.position
-
-        print("[%i] Max: %f | Min: %f" % (i, max_pos, min_pos))
+def get_distance(cell1, cell2):
+    """Calculated the distance between two cells."""
+    return abs(cell1.position - cell2.position)
 
 START_TIME = time.time()
 
-main()
+CELLS = []
+
+# Generate random initial cells
+for _ in range(const.INIT_CELL_COUNT):
+    CELLS.append(Cell.Cell(
+        np.random.uniform(-const.INIT_CELL_POSITION, const.INIT_CELL_POSITION),
+        np.random.uniform(0.0, const.INIT_CELL_VELOCITY),
+        np.random.uniform(0.0, const.INIT_CELL_MASS)))
+
+# Perform the evolution for a specified number of iterations
+for i in range(const.TARGET_ITERATIONS):
+    # New turn, all cells are ready again
+    for cel in CELLS:
+        cel.ready = True
+
+    i_src = 0
+    while i_src < len(CELLS):
+        if not CELLS[i_src].ready:
+            i_src += 1
+            continue
+
+        target_idx = None
+        target_distance = None
+
+        i_tgt = 0
+        while i_tgt < len(CELLS):
+            if CELLS[i_src].mass >= CELLS[i_tgt].mass and i_tgt != i_src:
+                distance = get_distance(CELLS[i_src], CELLS[i_tgt])
+                if target_idx is None or distance < target_distance:
+                    target_idx = i_tgt
+                    target_distance = distance
+
+            i_tgt += 1
+
+        if target_idx is not None:
+            if CELLS[i_src].attack(CELLS[target_idx]):
+                if CELLS[i_src].mass > const.CELL_MASS_LIMIT:
+                    CELLS.append(Cell.Cell(
+                        CELLS[i_src].position,
+                        CELLS[i_src].max_velocity,
+                        CELLS[i_src].mass - const.CELL_MASS_LIMIT))
+
+                    CELLS[i_src].mass = const.CELL_MASS_LIMIT
+
+                CELLS.pop(target_idx)
+                if target_idx < i_src:
+                    continue
+
+        i_src += 1
+
+    print("[%i] Count: %i" % (i, len(CELLS)))
+
+# Display survivor cells
+for cel in CELLS:
+    print("Position: %.2f | Velocity: %.3f | Mass: %.3f"
+          % (cel.position, cel.max_velocity, cel.mass))
 
 print("Done!", time.time() - START_TIME, "seconds")
